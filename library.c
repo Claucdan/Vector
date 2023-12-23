bb4a966f5d334129448e934e0c9b5f8b4423ba53
hhhh hgfh <dania.lukin@gmail.com>
2023-12-23 15:11:17 +0300

    Update push back Add function clear Add function empty

--- library.c
+++ library.c
@@ -1,16 +1,28 @@
 #include "library.h"
 #include "stdio.h"
 
-void constructor(vector* main,size_t type_of_data,size_t size){
-    main->type_of_data=type_of_data;
+void constructor(vector* main,size_t size){
     main->size=size;
     main->mass=(void**)malloc(size*sizeof(void*));
 }
 void push_back(vector* main,void* data){
     main->size++;
-    main->mass=(void*) realloc(main->mass,main->size*main->type_of_data);
+    main->mass=(void**) realloc(main->mass,main->size*sizeof(void*));
     main->mass[main->size-1]=data;
 }
 void* element_at(vector* main,int* index){
-    return &main->mass[*index];
+    return main->mass[*index];
+}
+void clear(vector* main){
+    for(int i=0;i<main->size;i++){
+        free(main->mass[i]);
+    }
+    free(main->mass);
+    main->size=0;
+}
+int empty(vector* main){
+    if(main->size==0)
+        return 0;
+    else
+        return 1;
 }
--- library.h
+++ library.h
@@ -5,15 +5,18 @@
 typedef struct vector vector;
 struct vector{
     void** mass;
-    size_t type_of_data;
     size_t size;
 };
 /*Constructor of vector:
  * For the constructor, you must enter the vector for which the constructor is used, the size of its data type, and its starting size*/
-void constructor(vector* main,size_t type_of_data,size_t size);
-
+void constructor(vector* main,size_t size);
+/*Push back element into vector
+ * For push back you should provide pointer to memory area*/
 void push_back(vector* main,void* data);
 
 void* element_at(vector* main,int* index);
 
+void clear(vector* main);
+
+int empty(vector* main);
 #endif //VECTOR_LIBRARY_H
