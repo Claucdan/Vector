ff9fb0e60d6f986345b6dfe2b1e14b91c75ba3e3
hhhh hgfh <dania.lukin@gmail.com>
2023-12-25 14:40:26 +0300

    all classical vector methods have been added

--- library.c
+++ library.c
@@ -1,17 +1,107 @@
 #include "library.h"
-#include "stdio.h"
 
-void constructor(vector* main,size_t size){
-    main->size=size;
-    main->mass=(void**)malloc(size*sizeof(void*));
+
+
+
+void constructor(vector* main,size_t capacity){
+    main->size=0;
+    main->max_size=capacity;
+    main->mass=(void**)malloc(capacity*sizeof(void*));
 }
-void push_back(vector* main,void* data){
-    main->size++;
-    main->mass=(void**) realloc(main->mass,main->size*sizeof(void*));
-    main->mass[main->size-1]=data;
+
+//Iterators
+
+void* begin(vector* main){
+    return main->mass[0];
 }
-void* element_at(vector* main,int* index){
+void* end(vector* main){
+    return main->mass[main->size-1];
+}
+const void* cbegin(vector* main){
+    return main->mass[0];
+}
+const void* cend(vector* main){
+    return main->mass[main->size-1];
+}
+bool empty(vector* main){
+    if(main->size==0)
+        return true;
+    else
+        return false;
+}
+
+//Capacity
+
+size_t size(vector* main){
+    return main->size;
+}
+size_t max_size(vector* main){
+    return main->max_size;
+}
+void resize(vector* main,size_t new_size){
+    main->max_size=new_size;
+    if(new_size<main->size){
+        for(int i=new_size;i<main->max_size;i++)
+            free(main->mass[i]);
+    }
+    main->mass=(void**)realloc(main->mass,new_size);
+    if(main->size>new_size)
+        main->size=new_size;
+}
+
+//Element access
+
+void* at(vector* main,const int* index){
     return main->mass[*index];
+}
+void* front(vector* main){
+    return main->mass[0];
+}
+void* back(vector* main){
+    return main->mass[main->size-1];
+}
+void** data(vector* main){
+    return main->mass;
+}
+
+//Modifiers
+
+void push_back(vector* main,void* data){
+    if(main->size==main->max_size){
+        main->max_size++;
+        main->size++;
+        main->mass=(void**) realloc(main->mass,main->max_size*sizeof(void*));
+        main->mass[main->max_size-1]=data;
+    }
+    else{
+        main->mass[main->size++]=data;
+    }
+}
+void pop_back(vector* main){
+    if(main->size!=0){
+        main->size--;
+        free(main->mass[main->size]);
+        main->mass= malloc(sizeof(void*)*main->size);
+    }
+}
+void insert(vector* main,const int* position,void* val){
+    main->size++;
+    if(main->size>main->max_size) {
+        main->mass = (void **) realloc(main->mass, main->size);
+        main->max_size++;
+    }
+    for(int i=main->size;i>*position;i++){
+        main->mass[i]=main->mass[i-1];
+    }
+    main->mass[*position]=val;
+}
+void erase(vector* main,const int* position,const int* count){
+    for(int i=*position;i<main->size-*count;i++){
+        if(i<*position+*count)
+            free(main->mass[i]);
+        main->mass[i]=main->mass[i+*count];
+    }
+    main->size-=*count;
 }
 void clear(vector* main){
     for(int i=0;i<main->size;i++){
@@ -20,9 +110,18 @@ void clear(vector* main){
     free(main->mass);
     main->size=0;
 }
-int empty(vector* main){
-    if(main->size==0)
-        return 0;
-    else
-        return 1;
+void emplace(vector* main,const int* position,void* val){
+    main->max_size++;
+    main->size++;
+    main->mass=(void**)realloc(main->mass, main->max_size);
+    for(int i=main->size;i>*position;i++){
+        main->mass[i]=main->mass[i-1];
+    }
+    main->mass[*position]=val;
+}
+void emplace_back(vector* main,void* val){
+    main->max_size++;
+    main->size++;
+    main->mass=(void**)realloc(main->mass, main->max_size);
+    main->mass[main->size-1]=val;
 }
--- library.h
+++ library.h
@@ -2,21 +2,77 @@
 #define VECTOR_LIBRARY_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 typedef struct vector vector;
 struct vector{
     void** mass;
     size_t size;
+    size_t max_size;
 };
+
+
+
+
 /*Constructor of vector:
  * For the constructor, you must enter the vector for which the constructor is used, the size of its data type, and its starting size*/
 void constructor(vector* main,size_t size);
-/*Push back element into vector
- * For push back you should provide pointer to memory area*/
+
+
+//Iterators
+
+/*Returns an iterator pointing to the first element in the vector*/
+void* begin(vector* main);
+/*Returns an iterator pointing to the theoretical element that follows the last element in the vector*/
+void* end(vector* main);
+/*Returns a constant iterator pointing to the first element in the vector*/
+const void* cbegin(vector* main);
+/*Returns a constant iterator pointing to the theoretical element that follows the last element in the vector*/
+const void* cend(vector* main);
+
+
+
+//Capacity
+
+/*Returns the number of elements in the vector*/
+size_t size(vector* main);
+/*Returns the maximum number of elements that the vector can hold*/
+size_t max_size(vector* main);
+/*Resizes the container so that it contains ‘new_size’ elements*/
+void resize(vector* main,size_t new_size);
+/*Returns whether the container is empty*/
+bool empty(vector* main);
+
+
+
+//Element access
+
+/*Returns a reference to the element at position ‘index’ in the vector*/
+void* at(vector* main,const int* index);
+/*Returns a reference to the first element in the vector*/
+void* front(vector* main);
+/*Returns a reference to the last element in the vector*/
+void* back(vector* main);
+/*Returns a direct pointer to the memory array used internally by the vector to store its owned elements*/
+void** data(vector* main);
+
+
+
+//Modifiers
+
+/*It pushes the elements into a vector from the back*/
 void push_back(vector* main,void* data);
-
-void* element_at(vector* main,int* index);
-
+/*It is used to pop or remove elements from a vector from the back*/
+void pop_back(vector* main);
+/*It inserts new element before the element at the specified position*/
+void insert(vector* main,const int* position,void* val);
+/*t is used to remove count of elements from a container from the specified position*/
+void erase(vector* main,const int* position,const int* count);
+/*It is used to remove all the elements of the vector container*/
 void clear(vector* main);
+/*It extends the container by inserting new element at position*/
+void emplace(vector* main,const int* position,void* val);
+/*It is used to insert a new element into the vector container, the new element is added to the end of the vector*/
+void emplace_back(vector* main,void* val);
 
-int empty(vector* main);
+
 #endif //VECTOR_LIBRARY_H
